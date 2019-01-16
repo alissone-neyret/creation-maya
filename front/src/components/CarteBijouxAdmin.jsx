d4dd3bff28ee1aaa8efa3f8573f5370bb26ac148
Alissone Neyret <alissone.neyret@gmail.com>
2019-01-16 23:10:04 +0100

    ajouter carte au catalogue

--- front/src/components/CarteBijouxAdmin.jsx
+++ front/src/components/CarteBijouxAdmin.jsx
@@ -50,24 +50,75 @@ class CarteBijouxAdmin extends Component {
     super(props);
     this.state = {
       bijoux,
+      ajout: false,
     }
+    this.ajoutCarte = this.ajoutCarte.bind(this);
   }
 
+  ajoutCarte() {
+    this.setState({
+      ajout: true,
+    })
+  }
 
   render() {
+    const { ajout } = this.state;
     return (
       <div className="CarteBijouxAdmin">
-        <Container>
-          <Row className="ligne-titre-bijoux">
-            <Col lg="5">
-              <h3>Administration du catalogue</h3>
-            </Col>
-          </Row>
-          <Row className="ligne-titre-bijoux">
-            <Col lg="3">
-              <Button className="bouton-ajout-article">Ajouter un article</Button>
-            </Col>
-          </Row>
+        {ajout === false
+          ?
+          <Container>
+            <Row className="ligne-titre-bijoux">
+              <Col lg="5">
+                <h3>Administration du catalogue</h3>
+              </Col>
+            </Row>
+            <Row className="ligne-titre-bijoux">
+              <Col lg="3">
+                <Button onClick={this.ajoutCarte} className="bouton-ajout-article">Ajouter un article</Button>
+              </Col>
+            </Row>
+            <Row className="ligne-carte-bijoux">
+              {this.state.bijoux.map((element) => (
+                <Col lg="4">
+                  <Card className="carte-bijoux">
+                    <CardImg className="carte-image-bijoux" top src={element.image} alt="Card image cap" />
+                    <CardBody className="corps-carte-bijoux">
+                      <CardTitle>{element.nom}</CardTitle>
+                      <CardSubtitle className="sous-titre-carte-bijoux">{element.prix}</CardSubtitle>
+                      <hr />
+                      <Button className="bouton-panier-bijoux" onClick={this.ajoutPanier}>Ajouter au panier</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+          :
+          <Container>
+            <Row className="ligne-titre-bijoux">
+              <Col lg="5">
+                <h3>Administration du catalogue</h3>
+              </Col>
+            </Row>
+            <Row className="ligne-titre-bijoux">
+              <Col lg="3">
+                <Button className="bouton-ajout-article">Ajouter un article</Button>
+              </Col>
+            </Row>
+            <Row className="ligne-nouvelle-carte">
+              <Col lg="4">
+                <Card className="carte-bijoux">
+                  <CardImg className="carte-image-bijoux" top src="" alt="Card image cap" />
+                  <CardBody className="corps-carte-bijoux">
+                    <CardTitle>titre</CardTitle>
+                    <CardSubtitle className="sous-titre-carte-bijoux">prix</CardSubtitle>
+                    <hr />
+                    <Button className="bouton-panier-bijoux" onClick={this.ajoutPanier}>Ajouter au panier</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
           <Row className="ligne-carte-bijoux">
             {this.state.bijoux.map((element) => (
               <Col lg="4">
@@ -83,7 +134,8 @@ class CarteBijouxAdmin extends Component {
               </Col>
             ))}
           </Row>
-        </Container>
+          </Container>
+      }
       </div>
     );
   }
