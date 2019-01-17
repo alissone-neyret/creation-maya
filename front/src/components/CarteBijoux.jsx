981cf0acbaea74b7130de1b7b21228df2295773c
Alissone Neyret <alissone.neyret@gmail.com>
2019-01-17 18:24:46 +0100

    ajout filtres

--- front/src/components/CarteBijoux.jsx
+++ front/src/components/CarteBijoux.jsx
@@ -19,9 +19,44 @@ class CarteBijoux extends Component {
       .then(data => {
         this.setState({
           bijoux: data,
+          prixMin: 0,
+          prixMax: 1000000000000,
         })
       })
+      this.filtreMin = this.filtreMin.bind(this);
+      this.filtreMiddle = this.filtreMiddle.bind(this);
+      this.filtreMax = this.filtreMax.bind(this);
+      this.filtreAll = this.filtreAll.bind(this);
   }
+
+  filtreMin() {
+    this.setState({
+      prixMin: 0,
+      prixMax: 50,
+    })
+  }
+
+  filtreMiddle() {
+    this.setState({
+      prixMin: 50,
+      prixMax: 100,
+    })
+  }
+  
+  filtreMax() {
+    this.setState({
+      prixMin: 100,
+      prixMax: 1000000000000,
+    })
+  }
+
+  filtreAll() {
+    this.setState({
+      prixMin: 0,
+      prixMax: 1000000000000,
+    })
+  }
+
 
   render() {
     console.log(this.state.bijoux)
@@ -32,10 +67,25 @@ class CarteBijoux extends Component {
             <Col lg="3">
               <h3>Les bijoux de Maya</h3>
             </Col>
-
+          </Row>
+          <Row className="justify-content-center">
+            <Col className="text-center" lg="3">
+              <Button className="bouton-filtre" onClick={this.filtreMin}>{"< 50€"}</Button>
+            </Col>
+            <Col className="text-center" lg="3">
+              <Button className="bouton-filtre" onClick={this.filtreMiddle}>{"50€-100€"}</Button>
+            </Col>
+            <Col className="text-center" lg="3">
+              <Button className="bouton-filtre" onClick={this.filtreMax}>{"> 100€"}</Button>
+            </Col>
+            <Col className="text-center" lg="3">
+              <Button className="bouton-filtre" onClick={this.filtreAll}>{"Tout voir"}</Button>
+            </Col>
           </Row>
           <Row className="ligne-carte-bijoux">
-            {this.state.bijoux[0] ? this.state.bijoux.map((element) => (
+            {this.state.bijoux[0] ? this.state.bijoux.filter(element => (
+              (element.prix >= this.state.prixMin) && (element.prix <= this.state.prixMax) 
+            )).map((element) => (
               <Col lg="4">
                 <Card className="carte-bijoux">
                   <CardImg className="carte-image-bijoux" top src={element.image} alt="Card image cap" />
